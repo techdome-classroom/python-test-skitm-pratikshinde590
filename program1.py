af9ee7ef2e2256b8db3cae37943497d62f8ed907
Pratik Shinde <pratik.shinde@techdome.net.in>
2024-01-11 19:05:43 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        brackets = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in brackets.values():
+                stack.append(char)
+            elif char in brackets.keys():
+                if not stack or stack.pop() != brackets[char]:
+                    return False
+            else:
+                return False
+
+        return not stack
     
 
 
